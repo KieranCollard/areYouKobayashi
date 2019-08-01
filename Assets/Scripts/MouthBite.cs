d9d9e0d0985974e4f7572cb182fcc57a27af9742
Kieran Collard <kierancollard@outlook.com>
2019-08-01 21:09:19 +1000

    Imported a water package for textures. Added basic water effect for the galss of water. Fixed issue with multiple click detects when eating the hotdog

--- Assets/Scripts/MouthBite.cs
+++ Assets/Scripts/MouthBite.cs
@@ -17,13 +17,11 @@ public class MouthBite : MonoBehaviour
     void Update()
     {
         //OVRInput.Update();
-        if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
         {
 
-            GameObject.Find("Clicked").GetComponent<UnityEngine.UI.Text>().text = "Detected click";
             foreach (Edible edible in edibles.Values)
             {
-                GameObject.Find("Overlap printout").GetComponent<UnityEngine.UI.Text>().text = "Eating";
                 edible.Bite();
             }
         }
@@ -45,24 +43,18 @@ public class MouthBite : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         var edibleScript = other.transform.GetComponent<Edible>();
-        Debug.Log("Attached edible script" + edibleScript);
         if (edibleScript != null)
         {
-            Debug.Log("Item entered mouth " + other.gameObject.name);
             edibles.Add(other.GetInstanceID(), edibleScript);
-            GameObject.Find("Grab Attempt").GetComponent<UnityEngine.UI.Text>().text = other.gameObject.name;
         }
     }
 
     //forget on exit
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("Item exited mouth " + other.name);
-        Debug.Log(edibles.Values);
         if (edibles.ContainsKey(other.GetInstanceID()))
         {
             edibles.Remove(other.GetInstanceID());
-            GameObject.Find("Grab Attempt").GetComponent<UnityEngine.UI.Text>().text = "";
         }
     }
 }
---
+++ Assets/Scripts/WaterBehaviour.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBehaviour : MonoBehaviour
+{
+    private Transform topOfWater;
+    private ParticleSystem waterParticles;
+
+    public int sloshSpeed = 60;
+    public int maxDifference = 25;
+    public int spillAngle = 45;
+
+    Vector3 startingRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        topOfWater = transform.Find("topOfWater");
+        waterParticles = topOfWater.Find("waterParticles").GetComponent<ParticleSystem>();
+
+        if(topOfWater == null)
+        {
+            Debug.Log("The top of water object was null. Make sure this prefab has a child named topOfWater");
+        }
+        if (waterParticles == null)
+        {
+            Debug.Log("The water particles was null. Make sure this prefab has a child named waterParticles that is a particle emitter");
+        }
+
+        startingRotation = this.transform.rotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //wate in a cup https://www.youtube.com/watch?v=OBnCd1bzCcw 
+       
+        //glass has icnorrect rotations
+        Vector3 localRotationCopy = transform.localRotation.eulerAngles;
+        localRotationCopy.x += 90;
+        Quaternion inverse = Quaternion.Inverse(Quaternion.Euler(localRotationCopy));
+        Vector3 finalRotation = Quaternion.RotateTowards(topOfWater.transform.localRotation, inverse, sloshSpeed * Time.deltaTime).eulerAngles;
+
+        finalRotation.x = ClampRotation(finalRotation.x, maxDifference);
+        finalRotation.y = ClampRotation(finalRotation.y, maxDifference);
+        
+
+        topOfWater.transform.localEulerAngles = finalRotation;
+        //if we are tipper over far enough start the particles
+        if(Mathf.Abs((startingRotation - this.transform.rotation.eulerAngles).x) > spillAngle)
+        {
+            if (waterParticles.isPlaying == false)
+            {
+                waterParticles.Play();
+            }
+        }
+        else
+        {
+            if(waterParticles.isPlaying)
+            {
+                waterParticles.Stop();
+            }
+        }
+    }
+
+    private float ClampRotation(float value, float difference)
+    {
+        float returnValue = 0.0f;
+        if(value > 180 )
+        {
+            returnValue = Mathf.Clamp(value, 360 - difference, 360);
+        }
+        else
+        {
+            returnValue = Mathf.Clamp(value, 0, difference);
+        }
+        return returnValue;
+    }
+}
